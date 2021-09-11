b2f06444b668b4ef05e12efd8492810326282139
Kobe Ruado <insidiae423@gmail.com>
2021-09-11 22:11:33 +0800

    memo demo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Header from "../components/Header";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
@@ -40,7 +41,7 @@ function App() {
     <h1>Loading</h1>
   ) : (
     <div className="tc">
-      <h1 className="f1">RoboFriends</h1>
+      <Header />
       {/* <button onClick={() => setCount(count + 1)}>Click Me!</button> */}
       <SearchBox searchChange={onSearchChange} />
       <Scroll>
